f5688ffaf7fd33f62b2e6c6d53129290dcbd3601
Евгений Котвицкий <ekotvizky@gmail.com>
2022-11-01 21:53:03 +0200

    radio background

--- src/app/radio/radio.component.ts
+++ src/app/radio/radio.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+interface RadioElement {
+  color: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-radio',
   templateUrl: './radio.component.html',
 })
 export class RadioComponent implements OnInit {
 
+  radioElements: Array<RadioElement> = [
+    { color:'blue', description: 'синий'},
+    { color:'red', description: 'красный'},
+    { color:'green', description: 'зеленый'},
+    { color:'yellow', description: 'золотой'}
+  ]
+
+  currentBackground:string = this.radioElements[0].color
+
   constructor() { }
+
+  setBackground(e:any) {
+    this.currentBackground = e.target.value
+  }
 
   ngOnInit(): void {
   }
