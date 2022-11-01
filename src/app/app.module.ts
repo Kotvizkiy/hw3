0c96bc7d390595c8b4fed52d2ff4b22ba669c388
Евгений Котвицкий <ekotvizky@gmail.com>
2022-11-01 21:12:44 +0200

    radio start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { RadioComponent } from './radio/radio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    RadioComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/radio/radio.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-radio',
+  templateUrl: './radio.component.html',
+})
+export class RadioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
