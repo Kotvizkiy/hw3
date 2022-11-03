1e885a845aaa70f059bc17763e12047e53f88007
Евгений Котвицкий <ekotvizky@gmail.com>
2022-11-03 18:07:10 +0200

    finish v0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { RadioComponent } from './radio/radio.component';
+import { PipesComponent } from './pipes/pipes.component';
+import { RemoveElemetnComponent } from './remove-elemetn/remove-elemetn.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    RadioComponent
+    RadioComponent,
+    PipesComponent,
+    RemoveElemetnComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/remove-elemetn/remove-elemetn.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-remove-elemetn',
+  templateUrl: './remove-elemetn.component.html'
+})
+export class RemoveElemetnComponent implements OnInit {
+
+  flagToggle: boolean = true;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
